ae1265d34db185046e3df2bb17422f3631001de5
AnastaciaVolkova <polarbolide@gmail.com>
2020-05-21 21:47:41 +0200

    feat: Improve output of messages

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -20,10 +20,6 @@ struct Goal {
 
 
 int main(int argc, char** argv){
-  // Util variables for messages.
-  stringstream message_iss;
-  string message;
-
   // Initialize the pick_objects node
   ros::init(argc, argv, "pick_objects_node");
   ros::NodeHandle n;
@@ -53,13 +49,8 @@ int main(int argc, char** argv){
     if (goal_it.name_ == "pickup"){
       srv.request.to_add = true;
       ros::service::waitForService(serv_name, -1);
-      if (!client.call(srv)){
-          message_iss << "Failed to call service " << serv_name;
-          message = message_iss.str();
-          ROS_ERROR(message.c_str());
-      } else {
-          ROS_INFO(srv.response.msg_feedback.c_str());
-      }
+      if (!client.call(srv))
+          ROS_ERROR("Failed to call service %s", serv_name.c_str());
     }
     // Define a position and orientation for the robot to reach
     goal.target_pose.pose.position.x = goal_it.x_;
@@ -67,9 +58,7 @@ int main(int argc, char** argv){
     goal.target_pose.pose.orientation.w = goal_it.w_;
 
     // Send the goal position and orientation for the robot to reach
-    message_iss << "Sending goal: (" << goal_it.x_ << ", " << goal_it.y_ << ")" << endl;
-    message = message_iss.str();
-    ROS_INFO(message.c_str());
+    ROS_INFO("Sending %s goal: (%s, %s)", goal_it.name_.c_str(), to_string(goal_it.x_).c_str(), to_string(goal_it.y_).c_str());
 
     ac.sendGoal(goal);
 
@@ -78,19 +67,14 @@ int main(int argc, char** argv){
 
     // Check if the robot reached its goal
     if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED){
-      message_iss << goal_it.name_ << " goal (" << goal_it.x_ << ", " << goal_it.y_ << ") is reached.";
-      message = message_iss.str();
-      ROS_INFO(message.c_str());
+      ROS_INFO("%s goal (%s, %s) is reached.", goal_it.name_.c_str(), to_string(goal_it.x_).c_str(), to_string(goal_it.y_).c_str());
+
       if (goal_it.name_ == "pickup")
         srv.request.to_add = false;
       else
         srv.request.to_add = true;
-      if (!client.call(srv)){
-        message_iss << "Failed to call service " << serv_name;
-        message = message_iss.str();
-        ROS_ERROR(message.c_str());
-      } else
-        ROS_INFO(srv.response.msg_feedback.c_str());
+      if (!client.call(srv))
+        ROS_ERROR("Failed to call service %s", serv_name.c_str());
     } else {
           if (ac.getState()==actionlib::SimpleClientGoalState::StateEnum::PENDING)
               ROS_INFO("PENDING");
