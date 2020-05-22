30f923367574ef6ddb7f320e2078eb7d9f7aef68
AnastaciaVolkova <polarbolide@gmail.com>
2020-05-22 22:44:59 +0200

    fix: Change roslaunch to rosrun. Insert space.

--- pick_objects.sh
+++ pick_objects.sh
@@ -4,6 +4,5 @@ xterm -e "source ./devel/setup.bash; roslaunch my_robot world.launch" &
 sleep 5
 xterm -e "source ./devel/setup.bash; roslaunch my_robot amcl.launch" &
 sleep 5
-xterm -e "source ./devel/setup.bash; roslaunch pick_objects pick_objects_node 0" &
-sleep5
-
+xterm -e "source ./devel/setup.bash; rosrun pick_objects pick_objects_node 0" &
+sleep 5
